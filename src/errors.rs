2b1e10802dfcfdf47d15f8b769a564e84d71bcc1
Marco Burla <mrc.burla@gmail.com>
2020-07-18 16:04:37 +0200

    rest errors added

---
+++ src/errors.rs
@@ -0,0 +1,41 @@
+use std::error::Error;
+
+use actix_web::{error, http::header, http::StatusCode, HttpResponse};
+use serde::{Deserialize, Serialize};
+use serde::export::fmt;
+use serde::export::Formatter;
+use serde_json::to_string;
+
+#[derive(Debug)]
+pub enum RestError {
+    PokemonNotFound(String),
+    UpstreamError(String),
+}
+
+impl fmt::Display for RestError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            RestError::PokemonNotFound(name) => write!(f, "Pokemon with name {} not found!", name),
+            RestError::UpstreamError(cause) => write!(f, "Something went wrong: {}", cause),
+        }
+    }
+}
+
+impl error::ResponseError for RestError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            RestError::PokemonNotFound(_) => StatusCode::NOT_FOUND,
+            RestError::UpstreamError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .json(ErrorResponseBody { message: self.to_string() })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ErrorResponseBody {
+    message: String
+}
