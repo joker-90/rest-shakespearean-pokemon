9d4aa809b5d240581a79b5990a1901b636178db5
Marco Burla <mrc.burla@gmail.com>
2020-07-18 15:43:38 +0200

    handler test added

--- src/handlers.rs
+++ src/handlers.rs
@@ -11,3 +11,19 @@ pub struct GetPokemonInfo {
 pub async fn get_pokemon(info: web::Path<GetPokemonInfo>) -> impl Responder {
     HttpResponse::Ok().json(Pokemon::new(&info.pokemon_name, "sss"))
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{App, test, web};
+
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_pokemon_get() {
+        let mut app = test::init_service(App::new()
+            .route("/pokemon/{pokemon_name}", web::get().to(get_pokemon))).await;
+        let req = test::TestRequest::with_uri("/pokemon/bulbasaur").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
+}
