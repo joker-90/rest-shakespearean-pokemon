0d9b0b457683c0d4afb7fb427e4684ac61214a79
Marco Burla <mrc.burla@gmail.com>
2020-07-18 21:37:23 +0200

    handler integration test assertion improved

--- src/handlers.rs
+++ src/handlers.rs
@@ -17,9 +17,13 @@ pub async fn get_pokemon(info: web::Path<GetPokemonInfo>, repositories: Data<Rep
     Ok(Json(pokemon))
 }
 
+
 #[cfg(test)]
 mod tests {
     use actix_web::{App, test, web};
+    use actix_web::http::StatusCode;
+    use actix_web::test::read_body;
+    use serde_json::Value;
 
     use super::*;
 
@@ -29,7 +33,34 @@ mod tests {
             .data(Repositories::default())
             .route("/pokemon/{pokemon_name}", web::get().to(get_pokemon))).await;
         let req = test::TestRequest::with_uri("/pokemon/bulbasaur").to_request();
+
         let resp = test::call_service(&mut app, req).await;
+
         assert!(resp.status().is_success());
+
+        let body = read_body(resp).await;
+
+        let result_pokemon = serde_json::from_slice::<Pokemon>(&body).unwrap();
+
+        assert_eq!(result_pokemon.name, "bulbasaur");
+        assert!(!result_pokemon.description.is_empty())
+    }
+
+    #[actix_rt::test]
+    async fn test_unknown_pokemon_get() {
+        let mut app = test::init_service(App::new()
+            .data(Repositories::default())
+            .route("/pokemon/{pokemon_name}", web::get().to(get_pokemon))).await;
+        let req = test::TestRequest::with_uri("/pokemon/unknown_pokemon").to_request();
+
+        let resp = test::call_service(&mut app, req).await;
+
+        assert_eq!(resp.status(), StatusCode::NOT_FOUND);
+
+        let body = read_body(resp).await;
+
+        let result_error = serde_json::from_slice::<Value>(&body).unwrap();
+
+        assert_eq!(result_error["message"].as_str(), Some("Pokemon with name unknown_pokemon not found!"));
     }
 }
